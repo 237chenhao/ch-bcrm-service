a1c32c9126e6d7c0ddf5538994d6f27a1e12575c
陈皓 <chenhao@isenruan.com>
2018-04-12 10:29:05 +0800

    feign extends

--- ch-service-core/src/main/java/com/ch/service/core/controller/Feign2Controller.java
+++ ch-service-core/src/main/java/com/ch/service/core/controller/Feign2Controller.java
@@ -2,10 +2,18 @@ package com.ch.service.core.controller;
 
 import com.ch.service.api.feign.FeignService;
 import com.ch.service.api.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author cj-ch
@@ -14,7 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class Feign2Controller implements FeignService {
-
+    final Logger log = LoggerFactory.getLogger(getClass());
+    @Autowired
+    private ServiceInstance serviceInstance;
     @Override
     public String feign4() {
         return "测试feign4的功能";
@@ -27,6 +37,14 @@ public class Feign2Controller implements FeignService {
 
     @Override
     public String feign6(@RequestBody User user) {
+        int sleepTime = new Random().nextInt(3000);
+        log.info("sleep time:{},in time:{}",sleepTime,LocalDateTime.now());
+        try {
+            TimeUnit.MILLISECONDS.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("/feign6,serviceId:{},metaData:{}",serviceInstance.getServiceId(),serviceInstance.getMetadata());
         return "测试feign6的功能,有请求体user="+user;
     }
 }
